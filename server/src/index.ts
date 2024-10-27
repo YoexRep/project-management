b3674691c72228b7d7a8a5be704a92b2bcc7574e
yoex <ytentertainment94@gmail.com>
2024-10-27 17:42:18 -0400

    Se agrego .config y app listen

--- server/src/index.ts
+++ server/src/index.ts
@@ -51,9 +51,9 @@ app.use("/teams", teamRoutes);
 
 
 //SERVER
-const port = process.env.PORT || 3000;
+const port = Number(process.env.PORT) || 3000;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
 
     console.log(`Server running on ports ${port}`);
 })
